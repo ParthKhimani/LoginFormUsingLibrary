c589f161d23a6ca6bc50cf003634ee0f0429e29b
nikhilnaagar <nnnagar1@gmail.com>
2023-05-22 11:33:43 +0530

    Login changes

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -5,7 +5,8 @@ const LoginForm = () => {
   return (
     <div className="Form">
       <h1 className="SignIn">Sign In</h1>
-      <label className="label">Email:</label>
+      <label className="label">Email</label>
+      <label className="star"> *</label>
       <CustomInput
         className="InputTag"
         type="email"
@@ -13,22 +14,45 @@ const LoginForm = () => {
         validationErrorMessage="*Invalid Email !"
         placeholder="abc@gmail.com"
       />
-      <label className="label">Password:</label>
+      <label className="label">Password</label>
+      <label className="star"> *</label>
       <CustomInput
         className="InputTag"
         type="password"
         name="password"
         validationErrorMessage="*Please use a strong password !"
         validationPattern={/^(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$/}
-        placeholder="password"
+        placeholder="Password"
       />
-      <CustomCheckbox
-        label="Keep me logged in"
-        value="LoggedIn"
-        className="CheckBox"
-      />
-      <a>Forgot Password ?</a>
+      
+      <div className="flex-container">
+        <CustomCheckbox
+          label="Keep me logged in"
+          value="LoggedIn"
+          className="CheckBox"
+        />
+        <a>Forgot Password ?</a>
+      </div>
+      
+
       <button>SIGN IN</button>
+
+      <div className="line">
+        <hr></hr>
+        <div>or Continue with</div>
+        <hr></hr>
+      </div>
+
+      <div className="logos">
+        <img src={"../public/google-log.png"} alt="google" /> 
+        <img src={"../public/apple-log.png"} alt="apple" />
+        <img src={"../public/pngwing-log.png"} alt="qb" />
+        <img src={"../public/xero-log.png"} alt="xero" />
+      </div>
+
+      <div className="link">
+        <p>Already have an account ? <a>Sign Up</a></p>
+      </div>
     </div>
   );
 };
